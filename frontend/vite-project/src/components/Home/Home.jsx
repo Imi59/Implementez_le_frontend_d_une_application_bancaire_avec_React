ac3b453814b5a0558f65982a0fdba96287adaf4e
Imane Baqua <imane.baqua@hotmail.fr>
2024-05-19 21:11:35 +0200

    composant features intégré

--- frontend/vite-project/src/components/Home/Home.jsx
+++ frontend/vite-project/src/components/Home/Home.jsx
@@ -1,5 +1,27 @@
 import Hero from "../Hero/Hero.jsx";
+import Feature from "../Features/Feature.jsx";
+// Donnés pour le chaque composant feature
+import featuresJson from "../../data/feature.json";
+
 const Home = () => {
-  return <Hero />;
+  const features = featuresJson.features;
+  return (
+    <>
+      <Hero />
+      <section className="features">
+        <h2 className="sr-only">Features</h2>
+        {features.map((feature, index) => (
+          <Feature
+            key={"feature" + index}
+            paragraph={feature.paragraph}
+            image={feature.image}
+            title={feature.title}
+            alt={feature.alt}
+          />
+        ))}
+      </section>
+    </>
+  );
 };
+
 export default Home;
